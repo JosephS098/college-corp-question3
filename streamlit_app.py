c4b6f3f7c849aa5f95a258fb54c5b8e07f05c485
JosephS098 <jsahagun@csustan.edu>
2025-10-28 11:53:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="QR Code Quiz 3", page_icon="🧠")
-st.title("🧩 Interactive College Corps Quiz!")
+st.title("🧩 College Corps Mid-Year Workshop!")
 st.markdown("<h3 style='color:#4CAF50;'>Let's see if you can solve this!</h3>", unsafe_allow_html=True)
 
 question = "How many campuses have a College Corps Program?"
